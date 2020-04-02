dc96d0cf875a61857772111bae4330e9792677b4
Vladimir Efremov <vefremov@gmail.com>
2020-04-02 22:18:56 +0200

    The version_str() method covered in Boost tests as well

--- tests/test_general_api.cpp
+++ tests/test_general_api.cpp
@@ -4,6 +4,7 @@
 
 #include <boost/test/unit_test.hpp>
 #include <iostream>
+#include <regex>
 
 BOOST_AUTO_TEST_SUITE(test_api)
 
@@ -19,6 +20,15 @@ BOOST_AUTO_TEST_CASE(test_message_valid)
 {
     string message = OTUS_HW::message();
     BOOST_CHECK(message == "Hello World!");
+}
+
+BOOST_AUTO_TEST_CASE(test_version_str)
+{
+    auto str = OTUS_HW::version_str();
+    std::regex re("^Version: \\d+\\.\\d+\\.\\d+$");
+    std::smatch m;
+    bool res = std::regex_match(str, m, re);
+    BOOST_CHECK(res);
 }
 
 }
