24218de836baa069a292ece0a38c0f982357e107
Vladimir Efremov <vefremov@gmail.com>
2020-04-02 15:53:48 +0200

    version_str covered by tests

--- tests/test_g_general_api.cpp
+++ tests/test_g_general_api.cpp
@@ -1,5 +1,6 @@
 #include "general_api.h"
 #include <string>
+#include <regex>
 
 #include <gtest/gtest.h>
 
@@ -13,4 +14,13 @@ TEST(general_api_tests, test_patch_valid)
 {
     int patch = OTUS_HW::version_patch();
     EXPECT_TRUE(patch > 0);
+}
+
+TEST(general_api_tests, test_version_str)
+{
+    auto str = OTUS_HW::version_str();
+    std::regex re("^Version: \\d+\\.\\d+\\.\\d+$");
+    std::smatch m;
+    bool res = std::regex_match(str, m, re);
+    EXPECT_TRUE(res);
 }
