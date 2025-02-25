828c4bb0db429fe1d4b9f2c663540e71fb1471af
Vladimir Efremov <efremov@pangea.ru>
2025-02-25 21:51:02 +0200

    Test connectivity.

--- test_g_general_api.cpp
+++ test_g_general_api.cpp
@@ -1,7 +1,16 @@
 #include "general_api.h"
 #include <string>
+#include <iostream>
 
 #include <gtest/gtest.h>
+
+struct TestFixtureName: public testing::Test {
+    void SetUp() override {
+        std::cout << "Fixture " << std::endl;
+        message = "Qq, buddy";
+    }
+    std::string message;
+};
 
 TEST(general_api_tests, test_message)
 {
@@ -12,5 +21,11 @@ TEST(general_api_tests, test_message)
 TEST(general_api_tests, test_patch_valid)
 {
     int patch = OTUS_HW::version_patch();
+    std::cout << "****** \n";
     EXPECT_TRUE(patch > 0);
+}
+
+TEST_F(TestFixtureName, test_with_fixture)
+{
+    std::cout << message << std::endl;
 }
